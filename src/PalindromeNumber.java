6a1ebcd7cd75f59b5e920bd3ef755fcd5e32bb56
Guanghao <zghaobac@gmail.com>
2016-06-23 07:45:36 +0800

    add more palindrome number solution

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -20,6 +20,44 @@ public class PalindromeNumber {
     System.out.println(x + " is palindrome ? " + isPalindrome(x));
     x = -101;
     System.out.println(x + " is palindrome ? " + isPalindrome(x));
+    
+    x = 100101001;
+    System.out.println(x + " is palindrome ? " + isPalindrome2(x));
+    x = 999010999;
+    System.out.println(x + " is palindrome ? " + isPalindrome2(x));
+    x = 10101011;
+    System.out.println(x + " is palindrome ? " + isPalindrome2(x));
+    x = -2147483647;
+    System.out.println(x + " is palindrome ? " + isPalindrome2(x));
+    x = -2147483648;
+    System.out.println(x + " is palindrome ? " + isPalindrome2(x));
+    x = 2147483647;
+    System.out.println(x + " is palindrome ? " + isPalindrome2(x));
+    x = 10;
+    System.out.println(x + " is palindrome ? " + isPalindrome2(x));
+    x = 1000000001;
+    System.out.println(x + " is palindrome ? " + isPalindrome2(x));
+    x = -101;
+    System.out.println(x + " is palindrome ? " + isPalindrome2(x));
+    
+    x = 100101001;
+    System.out.println(x + " is palindrome ? " + isPalindrome3(x));
+    x = 999010999;
+    System.out.println(x + " is palindrome ? " + isPalindrome3(x));
+    x = 10101011;
+    System.out.println(x + " is palindrome ? " + isPalindrome3(x));
+    x = -2147483647;
+    System.out.println(x + " is palindrome ? " + isPalindrome3(x));
+    x = -2147483648;
+    System.out.println(x + " is palindrome ? " + isPalindrome3(x));
+    x = 2147483647;
+    System.out.println(x + " is palindrome ? " + isPalindrome3(x));
+    x = 10;
+    System.out.println(x + " is palindrome ? " + isPalindrome3(x));
+    x = 1000000001;
+    System.out.println(x + " is palindrome ? " + isPalindrome3(x));
+    x = -101;
+    System.out.println(x + " is palindrome ? " + isPalindrome3(x));
   }
 
   public static boolean isPalindrome(int x) {
@@ -38,8 +76,6 @@ public class PalindromeNumber {
     while (times != 0 && times != 1) {
       start = (x / times) % 10;
       end = x % 10;
-      System.out.println("start : " + start + ", end : " + end 
-        + ", x : " + x + ", times : " + times);
       if (start != end) {
         return false;
       }
@@ -50,4 +86,24 @@ public class PalindromeNumber {
     return true;
   }
 
+  public static boolean isPalindrome2(int x) {
+    if (x < 0) {
+      return false;
+    }
+    int rx = ReverseInteger.reverse(x);
+    return rx == x;
+  }
+
+  public static boolean isPalindrome3(int x) {
+    if (x < 0) {
+      return false;
+    }
+    String s = String.valueOf(x);
+    for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+      if (s.charAt(i) != s.charAt(j)) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
