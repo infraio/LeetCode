e2c6e523616b0ea76b86772f035cd4cba0647d52
Guanghao <zghaobac@gmail.com>
2016-06-24 22:20:00 +0800

    add integer to roman

---
+++ src/IntegerToRoman.java
@@ -0,0 +1,26 @@
+
+public class IntegerToRoman {
+
+  public static void main(String[] args) {
+    System.out.println(intToRoman(10));
+    System.out.println(intToRoman(49));
+    System.out.println(intToRoman(99));
+    System.out.println(intToRoman(149));
+    System.out.println(intToRoman(499));
+    System.out.println(intToRoman(1001));
+    System.out.println(intToRoman(100100));
+  }
+
+  public static String intToRoman(int num) {
+    String str = "";    
+    String[] symbol={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};    
+    int[] value= {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    for (int i = 0; num != 0; i++) {
+      while (num >= value[i]) {
+        str += symbol[i];
+        num -= value[i];
+      }
+    }
+    return str;
+  }
+}
