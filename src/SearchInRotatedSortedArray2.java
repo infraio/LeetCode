65aecb2cace3cb51b3d28be2ba7a2219169796ba
Guanghao <zghaobac@gmail.com>
2016-07-13 21:55:29 +0800

    add solution for search in rotated sorted array 2 which allow duplicates

---
+++ src/SearchInRotatedSortedArray2.java
@@ -0,0 +1,44 @@
+
+public class SearchInRotatedSortedArray2 {
+
+  public static void main(String[] args) {
+    int[] nums = new int[] {5, 5, 7, 7, 1, 1, 3, 3};
+    System.out.println(search(nums, 0));
+    System.out.println(search(nums, 1));
+    System.out.println(search(nums, 2));
+    System.out.println(search(nums, 3));
+    System.out.println(search(nums, 4));
+    System.out.println(search(nums, 5));
+    System.out.println(search(nums, 6));
+    System.out.println(search(nums, 7));
+    System.out.println(search(nums, 8));
+    System.out.println(search(nums, 9));
+  }
+  
+  public static boolean search(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      if (target == nums[mid]) {
+        return true;
+      }
+      if (nums[left] == nums[mid]) {
+        left += 1;
+      } else if (nums[left] < nums[mid]) {
+        if (target >= nums[left] && target < nums[mid]) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
+        }
+      } else {
+        if (target > nums[mid] && target <= nums[right]) {
+          left = mid + 1;
+        } else {
+          right = mid - 1;
+        }
+      }
+    }
+    return false;
+  }
+}
